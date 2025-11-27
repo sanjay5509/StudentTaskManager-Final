f865600d07d0b2e6df0fc104d59394aa68d759f0
Pooja Agravat <pooja.beyondsy@gmail.com>
2025-11-27 17:05:12 +0530

    FIX: Added PaginatedTaskListViewModel file which was missing.

--- Models/PaginatedTaskListViewModel.cs
+++ Models/PaginatedTaskListViewModel.cs
@@ -1,21 +1,18 @@
 ﻿
-
 using System.Collections.Generic;
 
 namespace StudentTaskManager.Models
 {
     public class PaginatedTaskListViewModel
     {
-        
-        public List<TaskModel> Tasks { get; set; }
-        public List<Category> Categories { get; set; }
+       
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+       
+        public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
 
         public int PageNumber { get; set; }
-
-        
         public int TotalPages { get; set; }
-
-        
         public int TotalTasks { get; set; }
     }
 }
