ea4e61647067a4407f8ad1872a3222d07e9ccdb7
Pooja Agravat <pooja.beyondsy@gmail.com>
2025-11-27 18:03:34 +0530

    Final UX Polish: Implemented responsive mobile sidebar toggle and theme alignment.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    // active-sidebar class ko add/remove karega
+    sidebar.classList.toggle('active-sidebar');
+}
